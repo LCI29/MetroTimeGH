712d788910c703817c30e09e33a291af60e65c1b
Clement Lenskiy <blind-mind@yandex.ru>
2021-11-10 00:54:06 +0300

    Version 1.7. InfoFragment.kt implemented. Now uses Internet permission

---
+++ app/src/main/java/ru/clementl/metrotimex/ui/fragments/InfoFragment.kt
@@ -0,0 +1,36 @@
+package ru.clementl.metrotimex.ui.fragments
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import ru.clementl.metrotimex.BuildConfig
+import ru.clementl.metrotimex.R
+
+class InfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        return requireActivity().layoutInflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.text_version).text =
+            resources.getString(R.string.version_text, BuildConfig.VERSION_NAME)
+        view.findViewById<ImageView>(R.id.metrotest_img).setOnClickListener {
+            val ref = "https://play.google.com/store/apps/details?id=com.wannagoflorida.metrotest"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(ref))
+            startActivity(intent)
+        }
+    }
+}
